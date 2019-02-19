f62edeceef59072d4cfa70e87df3316b70768ca6
fsqWendy <1185745180@qq.com>
2019-02-19 17:25:08 +0800

    fsq

---
+++ project/src/components/company profile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="culture">
+    <div style="float: left;margin-right: 30px;">
+      <img src="../assets/images/comindex/slide1.jpg" style="width:450px;height:300px;" class="img-responsive" />
+    </div>
+    <div style="margin-right: 30px;">
+      <div >
+        <h1>公司简介</h1>
+      </div>
+      <div><em>Company profile</em></div>
+
+      <p class="large">杭州聚而禾土地规划设计咨询有限公司创建于2016年6月,位于杭州西湖景区边的黄
+        龙商圈。具有土地规划、摄影測量与遥感、地理信息系统工程、工程测量、不动产测
+        绘、不动产登记代理等行业资质。</p>
+      <p class="large">
+        公司致力于土地调查评价、规划设计、政策咨询、项目前期服务等业务领域,兼营
+        地理信息系统的技术开发、测绘技术、农林业调查与规划设计,服务于各级国土、农
+        业、林业、交通、水利等政府机关和企事业单位。公司具高级技术职称人员占20%
+        博士硕士学历专业技术人员占40%,拥有完善的技术设备支持,技术实力雄厚。公司
+        注重客户服务质量,采用严格的专业质量管理体系公司注重客户满意度,通过导入“
+        卓越绩效”管理机制,提升产品体验。客户满意率达100%。</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "company profile"
+    }
+</script>
+
+<style scoped>
+
+</style>
