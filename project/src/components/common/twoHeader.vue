019060b9e97dbbb5b97808f2b3a388caaaa14859
gany <1049783847@qq.com>
2019-02-19 16:07:14 +0800

    gy

---
+++ project/src/components/common/twoHeader.vue
@@ -0,0 +1,157 @@
+<template>
+  <div style="width: 1617px;margin: 0 auto;">
+    <div class="clearfix" style="height:270px;">
+      <div class="bannerHover1">
+
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+        >
+          <div class="logo"><img src="../../assets/images/comindex/logo_white.png"/></div>
+          <el-submenu index="1" >
+            <template slot="title">走进聚而禾</template>
+            <el-menu-item :index="'1-'+index"  v-for="(item,index) in oneDetail " :key="index"  @click="that.$jk.getDescribe('/moreNews')">{{item.value}}</el-menu-item>
+
+
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">业务体系</template>
+            <el-menu-item :index="'2-'+index"  v-for="(item,index) in twoDetail " :key="index">{{item.value}}</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3">招贤纳士</el-menu-item>
+          <el-menu-item index="4">联系我们</el-menu-item>
+        </el-menu>
+
+      </div>
+    </div>
+
+    <div class="left_nav">
+      <h2>业务体系</h2>
+      <ul class="lefu">
+        <li><a href="">业务领域</a></li>
+        <li><a href="">项目成果</a></li>
+        <li><a href="">产品展示</a></li>
+        <li><a href="">解决方案</a></li>
+        <li><a href="">技术支持</a></li>
+      </ul>
+    </div>
+
+    <div style="height:300px;background: black;margin-top: 20px;">
+
+    </div>
+
+  </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: "two-hearder",
+      data() {
+        return {
+          activeIndex: '1',
+          listData: [],
+          oneDetail: [
+            {value: '公司简介', path: 'index'},
+            {value: '公司文化', path: 'film', id: 0},
+            {value: '组织架构', path: 'login'},
+            {value: '资质荣誉', path: 'about_culture', id: 1},
+            {value: '管理团队', path: 'join_us'},
+            {value: '团队风采', path: 'contact_us'},
+          ],
+          twoDetail: [
+            {value: '业务领域', path: 'index'},
+            {value: '项目成果', path: 'film', id: 0},
+            {value: '产品展示', path: 'login'},
+            {value: '解决方案', path: 'about_culture', id: 1},
+            {value: '技术支持', path: 'join_us'},
+
+          ],
+        }
+      },
+      methods: {
+
+
+      }
+    }
+</script>
+
+<style scoped>
+  .nav {
+    border-bottom: solid 1px #e6e6e6;
+    box-sizing: border-box;
+  }
+  .nav ul{
+    list-style: none;
+    width:1200px;
+    margin: 0 auto;
+  }
+  .nav li{
+    float:left;
+    margin-right: 60px;
+    color:#636363;
+    height:55px;
+    line-height: 55px;
+    cursor: pointer;
+    box-sizing: border-box;
+  }
+  .nav .active{
+    border-bottom:2px solid #32cd32 !important;
+  }
+  .sNav {
+    height: 101px;
+    width: 45%;
+    background: white;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 7.5px;
+    border-radius: 3px;
+  }
+  .left_nav h2{
+    font-size: 22px;
+    height: 70px;
+    background: white;
+    line-height:70px;
+  }
+  .left_nav{
+
+    width: 300px;
+    height:590px;
+    background: rgba(252, 252, 252, 0.86);
+    border: 1px solid #dedede;
+
+  }
+  h2{
+    font-size: 22px;
+    text-align: center;
+    font-weight: bold;
+    background: white;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+  .lefu{
+    width:100%;
+    margin-left: -40px;
+  }
+  .lefu li{
+    height: 60px;
+    line-height: 60px;
+
+  }
+  .lefu li:hover{
+    background: #eeeeee;
+
+  }
+  .lefu li a{
+    width:100%;
+    display: inline-block;
+    text-align: center;
+  }
+  .lefu li a:hover{
+    text-decoration: none;
+  }
+</style>
