ac08f9dc0c65d9d44169d410c889e879a24cedf7
fsqWendy <1185745180@qq.com>
2019-02-19 17:22:22 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	project/src/assets/css/common.css
    #	project/src/components/comIndex.vue

--- project/src/components/company culture.vue
+++ project/src/components/company culture.vue
@@ -1,5 +1,17 @@
 <template>
+  <div class="culture" style="">
+    <!--<div  style="margin-left: 130px;line-height: 30px;color:black;">
+      <h2 class="h2" style="margin-top:-100px;">简单</h2>
+      <h4 class="h4" style=" text-align: left"> 大道至简。注重办事精简、力求效率，追求客户“最多跑一次”的目标。</h4>
+      <h2 class="h2">互助</h2>
+      <h4 class="h4" style=" text-align: left">  于内团结互助，于外合作相助。</h4>
+      <h2 class="h2" style="margin-left: 500px;">精诚</h2>
+      <h4 class="h4" style=" text-align: left;margin-left:500px;" >  敬业是我们的态度，精业是我们的追求，立诚立信使我们的承诺。</h4>
+      <h2 class="h2" style="margin-left: 500px;">奋进</h2>
+      <h4 class="h4" style=" text-align: left;margin-left:500px;">  脚踏实地，精益求精，携手共进是我们的信念。</h4>
+    </div>-->
 
+  </div>
 </template>
 
 <script>
